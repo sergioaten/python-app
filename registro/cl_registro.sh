74c8e00ae63c7faec598295c032dd5af86d3173f
richifor <richi.instructor.formacion@gmail.com>
2023-05-25 08:52:53 +0000

    modificado cliente registro

--- registro/cl_registro.sh
+++ registro/cl_registro.sh
@@ -1,45 +1,73 @@
 #!/bin/bash
-# Función para mostrar el uso del script
-mostrar_uso() {
-    echo "Uso: $0 [opciones]"
-    echo "Opciones:"
-    echo "  -a, --agregar     Agregar una cuenta de usuario"
-    echo "  -c, --consultar   Consultar la existencia de una cuenta de usuario"
-    exit 1
+
+BASE_URL="http://localhost:6900"
+
+function make_post_request() {
+    local endpoint="$1"
+    local data="$2"
+    curl -s -H "Content-Type: application/json" -X POST -d "$data" "$BASE_URL$endpoint"
 }
 
-# Función para agregar una cuenta de usuario
-agregar_cuenta() {
-    nombre=$1
-    apellido=$2
-    registro_response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"nombre": "'$nombre'", "apellidos": "'$apellido'"}' http://localhost:6900/registro)
-    token=$(echo $registro_response | jq -r '.token')
-    echo "Token de la cuenta registrada: $token"
+function make_get_request() {
+    local endpoint="$1"
+    curl -s "$BASE_URL$endpoint"
 }
 
-# Función para consultar la existencia de una cuenta de usuario
-consultar_cuenta() {
-    nombre=$1
-    token=$2
-    verificar_response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"nombre": "'$nombre'", "token": "'$token'"}' http://localhost:6900/verificar)
-    existe=$(echo $verificar_response | jq -r '.existe')
+function registro_usuario() {
+    read -p "Ingrese el nombre: " nombre
+    read -p "Ingrese los apellidos: " apellidos
 
-    if [[ $existe == "true" ]]; then
-        echo "La cuenta con el token $token existe"
-    else
-        echo "La cuenta con el token $token no existe"
-    fi
+    local data="{\"nombre\":\"$nombre\",\"apellidos\":\"$apellidos\"}"
+    make_post_request "/registro" "$data"
 }
 
-# Parsear los argumentos de línea de comandos
-case $1 in
-     -a|--agregar)
-		agregar_cuenta $2 $3
-      ;;
-     -c|--consultar)
-		consultar_cuenta $2 $3
-     ;;
-     *)
-       mostrar_uso
-    ;;
-esac
+function verificar_cuenta() {
+    read -p "Ingrese el nombre: " nombre
+    read -p "Ingrese el token: " token
+
+    local data="{\"nombre\":\"$nombre\",\"token\":\"$token\"}"
+    make_post_request "/verificar" "$data"
+}
+
+function obtener_usuarios() {
+    make_get_request "/usuarios"
+}
+
+function obtener_tokens() {
+    make_get_request "/tokens"
+}
+
+function obtener_basedatos() {
+    make_get_request "/basedatos"
+}
+
+function mostrar_menu() {
+    echo "========== MENÚ =========="
+    echo "1. Registrar usuario"
+    echo "2. Verificar cuenta"
+    echo "3. Obtener usuarios"
+    echo "4. Obtener tokens"
+    echo "5. Obtener base de datos"
+    echo "0. Salir"
+    echo "=========================="
+}
+
+while true; do
+    mostrar_menu
+    read -p "Ingrese una opción: " opcion
+    echo
+
+    case $opcion in
+        1) registro_usuario ;;
+        2) verificar_cuenta ;;
+        3) obtener_usuarios ;;
+        4) obtener_tokens ;;
+        5) obtener_basedatos ;;
+        0) break ;;
+        *) echo "Opción inválida. Por favor, seleccione una opción válida." ;;
+    esac
+
+    echo
+done
+
+echo "¡Hasta luego!"
